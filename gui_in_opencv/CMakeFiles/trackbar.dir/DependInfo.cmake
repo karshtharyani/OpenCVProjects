501b7c3ba379a068aca14917c6aaacbe87911dbe
karsh@shakey <ktharyan@andrew.cmu.edu>
2017-07-26 00:48:48 +0530

    Initialize OpenCVProjects Repository

---
+++ gui_in_opencv/CMakeFiles/trackbar.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/karsh/Documents/opencv_projects/gui_in_opencv/source.cpp" "/home/karsh/Documents/opencv_projects/gui_in_opencv/CMakeFiles/trackbar.dir/source.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/karsh/opencv/opencv/release"
+  "/home/karsh/opencv/opencv/include"
+  "/home/karsh/opencv/opencv/include/opencv"
+  "/home/karsh/opencv/opencv/modules/core/include"
+  "/home/karsh/opencv/opencv/modules/flann/include"
+  "/home/karsh/opencv/opencv/modules/imgproc/include"
+  "/home/karsh/opencv/opencv/modules/ml/include"
+  "/home/karsh/opencv/opencv/modules/objdetect/include"
+  "/home/karsh/opencv/opencv/modules/photo/include"
+  "/home/karsh/opencv/opencv/modules/video/include"
+  "/home/karsh/opencv/opencv/modules/viz/include"
+  "/home/karsh/opencv/opencv/modules/dnn/include"
+  "/home/karsh/opencv/opencv/modules/imgcodecs/include"
+  "/home/karsh/opencv/opencv/modules/shape/include"
+  "/home/karsh/opencv/opencv/modules/videoio/include"
+  "/home/karsh/opencv/opencv/modules/highgui/include"
+  "/home/karsh/opencv/opencv/modules/superres/include"
+  "/home/karsh/opencv/opencv/modules/ts/include"
+  "/home/karsh/opencv/opencv/modules/features2d/include"
+  "/home/karsh/opencv/opencv/modules/calib3d/include"
+  "/home/karsh/opencv/opencv/modules/stitching/include"
+  "/home/karsh/opencv/opencv/modules/videostab/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ image_manipulations/CMakeFiles/image_gray.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_gray.dir/source.cpp.o"
+  "image_gray.pdb"
+  "image_gray"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_gray.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
